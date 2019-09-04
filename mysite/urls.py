a9c4c87885f4119418a751d894725e14a7c4044b
VARUN KUMAR <varunjha089@gmail.com>
2019-09-05 00:05:55 +0530

    Updating User Accounts with Forms

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,7 +22,8 @@ from personal.views import (
 from account.views import (
     registration_view,
     logout_view,
-    login_view
+    login_view,
+    account_view
 )
 
 urlpatterns = [
@@ -31,4 +32,5 @@ urlpatterns = [
     path('register/', registration_view, name="register"),
     path('logout/', logout_view, name="logout"),
     path('login/', login_view, name="login"),
+    path('account/', account_view, name="account"),
 ]
