d5f72aec30f8fd318cda47ddbb7fa7ae6f1a5344
VARUN KUMAR <varunjha089@gmail.com>
2019-09-06 20:29:56 +0530

    Modeling the Blog App using Reddit.com

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -37,6 +37,7 @@ urlpatterns = [
     path('logout/', logout_view, name="logout"),
     path('login/', login_view, name="login"),
     path('account/', account_view, name="account"),
+    path('blog/', include('blog.urls', 'blog')),
 
     # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/',
