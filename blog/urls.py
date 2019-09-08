523a8891a8d53bfe6c0c764d33d26b4f3acba2c7
VARUN KUMAR <varunjha089@gmail.com>
2019-09-08 17:12:57 +0530

    Updating Blog Posts using a Django Form
    Django Form (Images)pdating Blog Posts using a

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from blog.views import (
     create_blog_view,
-    detail_blog_view
+    detail_blog_view,
+    edit_blog_view
 )
 
 app_name = 'blog'
@@ -10,4 +11,5 @@ app_name = 'blog'
 urlpatterns = [
     path('create', create_blog_view, name="create"),
     path('<slug>/', detail_blog_view, name="detail"),
+    path('<slug>/edit/', edit_blog_view, name="edit"),
 ]
