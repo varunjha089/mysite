0d1c3561534475b15c634b16f86e5caf16907837
VARUN KUMAR <varunjha089@gmail.com>
2019-09-08 21:25:21 +0530

    Blog Search Bar (Django Q Lookup)

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from blog.models import BlogPost
 from blog.forms import CreateBlogPostForm, UpdateBlogPostForm
 from account.models import Account
+from django.db.models import Q
 
 
 # Create your views here.
@@ -59,3 +60,18 @@ def edit_blog_view(request, slug):
     context['form'] = form
 
     return render(request, 'blog/edit_blog.html', context)
+
+
+def get_blog_queryset(query=None):
+    queryset = []
+    queries = query.split(" ")
+    for q in queries:
+        posts = BlogPost.objects.filter(
+            Q(title__contains=q) |
+            Q(body__icontains=q)
+        ).distinct()
+        for post in posts:
+            queryset.append(post)
+
+    # create unique set and then convert to list
+    return list(set(queryset))
--- personal/views.py
+++ personal/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from blog.models import BlogPost
 from operator import attrgetter
+from blog.views import get_blog_queryset
 
 
 def home_screen_view(request):
     context = {}
-    blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
-    context['blog_posts'] = blog_posts
 
+    query = ""
+    if request.GET:
+        query = request.GET['q']
+        context['query'] = str(query)
+    blog_posts = sorted(get_blog_queryset(query), key=attrgetter('date_updated'), reverse=True)
+    context['blog_posts'] = blog_posts
     return render(request, "personal/home.html", context)
