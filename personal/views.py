b6e2556a246137f9cd4c3c1c8015fde356a863b4
VARUN KUMAR <varunjha089@gmail.com>
2019-09-08 19:26:33 +0530

    Blog Feed Like Reddit.com

--- personal/views.py
+++ personal/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from account.models import Account
+from blog.models import BlogPost
+from operator import attrgetter
 
 
 def home_screen_view(request):
     context = {}
-
-    accounts = Account.objects.all()
-    context['accounts'] = accounts
+    blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
+    context['blog_posts'] = blog_posts
 
     return render(request, "personal/home.html", context)
